a766d45ee3b0bbded5417a329440c75b113786c6
Glauco Lugheri <lugheri@live.com>
2023-05-09 22:38:35 -0300

    Setup dev script

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,23 @@
 import http from 'http';
+
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json({limit:'250mb'}))
+app.use(express.static(path.resolve(__dirname,'../public')));
+app.use(express.urlencoded({extended:true, limit:'250mb'}));
+
+
+
+const httpServer = http.createServer(app);
+
+
+const startUp = async() => {
+  httpServer.listen(4000,()=>console.log('😀 Backend Platform v4 - online!'));
+}
+startUp()
